09ac04c89aafc9926e35baed988d0032ced0ead7
Lucineia Pacheco <lucineia.pacheco@ifro.edu.br>
2025-04-16 13:19:22 -0400

    Add files via upload

---
+++ exemplos_em_sala.js
@@ -0,0 +1,14 @@
+
+// console.log(process.argv)
+const argumentos = process.argv.slice(2)
+
+
+
+let valorA = parseInt(argumentos[1])
+let valorB  = parseInt(argumentos[1])
+
+let resultado = valorA + valorB
+
+console.log("A + B = ", resultado)
+
+
